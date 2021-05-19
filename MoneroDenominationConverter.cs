1abc5bf489059c187b701a8c2e97f84bc4759e8f
Agorist-Action <agorist_action@protonmail.com>
2021-05-18 19:51:57 -0500

    Add MoneroDenominationConverter

---
+++ MoneroDenominationConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneroTransactionSniffer
+{
+    sealed class MoneroDenominationConverter
+    {
+        private const double PiconerosPerMonero = 1_000_000_000_000;
+
+        public static double PiconeroToMonero(ulong piconero)
+        {
+            return piconero / PiconerosPerMonero;
+        }
+    }
+}
